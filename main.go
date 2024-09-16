3f692e86174ae0fad069b733efa231f2e8a184a5
kornchetchen <inpicblog@gmail.com>
2024-09-16 09:18:01 +0700

    save

--- main.go
+++ main.go
@@ -5,6 +5,16 @@ import (
 	"log"
 	"net/http"
 )
+funchelloHandler( w http.ResponseWriter , r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found",http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported",http.StatusNotFound)
+	}
+}
+
 func main() {
 	fileServer := http.fileServer(http.Die("./static"))
 	http.Handle("/".fileServer)
@@ -13,6 +23,8 @@ func main() {
 
 	fmt.Printf("Starting server at prot 8080\n")
 	if err := http.ListernAndServer(":8080",nil); err !=nil {
-		log.Fatal(err)
+		log.Fatal(err); err !=nill {
+			log.Fatal(err)
+		}
 	}
 }
